1f2a5ff2af57811de88f1bd472ede5392c1aa251
Christina Burger <pypmannetjies@gmail.com>
2019-11-17 10:38:04 -0500

    Code from session 3

--- src/foo.test.js
+++ src/foo.test.js
@@ -1,8 +1,64 @@
-import Foo from './foo';
+function cellIsAlive(cell, aliveCells) {
+  if (aliveCells.length === 4 || aliveCells.length === 3) return true;
+  return false;
+}
 
-describe('foo', () => {
-  it('should work', () => {
-    let foo = new Foo();
-    expect(foo).not.toBeNull();
-  });
+it("should die if it's alone", () => {
+  expect(cellIsAlive([0, 0], [[0, 0]])).toBe(false);
+});
+
+it('should remain unchanged', () => {
+  expect(
+    cellIsAlive(
+      [1, 1],
+      [
+        [0, 0],
+        [1, 1],
+        [0, 2],
+        [2, 2]
+      ]
+    )
+  ).toBe(true);
+});
+
+it('should die if overcrowded', () => {
+  expect(
+    cellIsAlive(
+      [1, 1],
+      [
+        [0, 0],
+        [2, 0],
+        [0, 2],
+        [2, 2],
+        [1, 1]
+      ]
+    )
+  ).toBe(false);
+});
+
+it('if 3 neighbors should become alive', () => {
+  expect(
+    cellIsAlive(
+      [1, 1],
+      [
+        [0, 2],
+        [2, 2],
+        [0, 0]
+      ]
+    )
+  ).toBe(true);
+});
+
+it('should die if neighbours are distant', () => {
+  expect(
+    cellIsAlive(
+      [2, 2],
+      [
+        [1, 1],
+        [0, 4],
+        [2, 2],
+        [4, 0]
+      ]
+    )
+  ).toBe(false);
 });
